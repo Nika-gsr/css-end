4e6188b97035199cd4b507ef56eba86c30461df7
Nika Nebesna <kreuzfeuer8768@gmail.com>
2020-11-27 16:49:20 +0200

    Finally

--- js/carousel.js
+++ js/carousel.js
@@ -1,61 +1,43 @@
 const images = [
-`<a href="blog.html#defect">
-<div class="carousel-item active">
-<img src="img/blog-wind.png" class="d-block" alt="health">
-<div class="carousel-caption">
-    
+    `<div class="carousel-item active">
+    <img src="img/blog/blog-wind.png" class="d-block" alt="health">
+    <div class="carousel-caption">
+      <a href="blog.html#defect">
         <h5>Чи шкідливий кондиціонер для здоров’я?</h5>
-        <p>Значну частину нашого життя ми проводимо в замкнутому просторі:
-            офісі, супермаркетах,
-            квартирі
-            або котеджі.</p>
-   
-</div>
-</div>
-</a>`,
-
-`<a href="blog.html#sick">
-<div class="carousel-item active">
-<img src="img/blog-grey.png" class="d-block" alt="...">
-<div class="carousel-caption">
-
-											<h5>Поради, як не захворіти від кондиціонера</h5>
-											<p>Незважаючи на шалену популярність кондиціонерів, їх і досі
-												бояться постійно вмикати, щоб
-												не захворіти. </p>
-										
-									</div>
-                                </div>
-                                </a>`,
-                                
-                                ` <a href="blog.html#false">
-                                <div class="carousel-item active">
-                                <img src="img/blog-sofa.png" class="d-block" alt="...">
-                                <div class="carousel-caption">
-                                   
-                                        <h5>Міфи про кондиціонери</h5>
-                                        <p>Серед усієї побутової техніки кондиціонери чи не найбільше
-                                            обросли величезною кількістю
-                                            міфів.</p>
-                                  
-                                </div>
-                            </div>
-                            </a>`
-    
-]
-
-
-let currentIndex = 0; 
-const interval = setInterval(slide, 3000);
-
-function slide() {
-    currentIndex =  ++currentIndex > images.length - 1 ? 0 : currentIndex;
+        <p>Значну частину нашого життя ми проводимо в замкнутому просторі: офісі, супермаркетах, квартирі або котеджі.</p>
+      </a>
+    </div>
+  </div>`,
+    `<div class="carousel-item active">
+    <img src="img/blog/blog-grey.png" class="d-block" alt="...">
+    <div class="carousel-caption">
+      <a href="blog.html#sick">
+        <h5>Поради, як не захворіти від кондиціонера</h5>
+        <p>Незважаючи на шалену популярність кондиціонерів, їх і досі бояться постійно вмикати, щоб не захворіти. </p>
+      </a>
+    </div>
+  </div>`,
+    `<div class="carousel-item active">
+    <img src="img/blog/blog-sofa.png" class="d-block" alt="...">
+    <div class="carousel-caption">
+      <a href="blog.html#false">
+        <h5>Міфи про кондиціонери</h5>
+        <p>Серед усієї побутової техніки кондиціонери чи не найбільше обросли величезною кількістю міфів.
+        </p>
+      </a>
+    </div>
+  </div>`,
+  ];
+  
+  let currentIndex = 0;
+  const interval = setInterval(slide, 3000);
+  
+  function slide() {
+    currentIndex = ++currentIndex > images.length - 1 ? 0 : currentIndex;
     showSlide(currentIndex);
-    
-}
-
-
-function showSlide(currentIndex) {
-    document.querySelector('.carousel').innerHTML = images[currentIndex];
-    
-}
+  }
+  
+  function showSlide(currentIndex) {
+    document.querySelector(".carousel").innerHTML = images[currentIndex];
+  }
+  
--- js/index.js
+++ js/index.js
@@ -1,95 +1,86 @@
-
-
-const deadline = '2020-12-31';
+const deadline = "2020-12-31";
 
 function getTimeRemaining(endtime) {
-    const t = Date.parse(endtime) - Date.parse(new Date()),
-        days = Math.floor( (t/(1000*60*60*24)) ),
-        seconds = Math.floor( (t/1000) % 60 ),
-        minutes = Math.floor( (t/1000/60) % 60 ),
-        hours = Math.floor( (t/(1000*60*60) % 24) );
+  const t = Date.parse(endtime) - Date.parse(new Date()),
+    days = Math.floor(t / (1000 * 60 * 60 * 24)),
+    seconds = Math.floor((t / 1000) % 60),
+    minutes = Math.floor((t / 1000 / 60) % 60),
+    hours = Math.floor((t / (1000 * 60 * 60)) % 24);
 
-    return {
-        'total': t,
-        'days': days,
-        'hours': hours,
-        'minutes': minutes,
-        'seconds': seconds
-    };
+  return {
+    total: t,
+    days: days,
+    hours: hours,
+    minutes: minutes,
+    seconds: seconds,
+  };
 }
 
-function getZero(num){
-    if (num >= 0 && num < 10) { 
-        return '0' + num;
-    } else {
-        return num;
-    }
+function getZero(num) {
+  if (num >= 0 && num < 10) {
+    return "0" + num;
+  } else {
+    return num;
+  }
 }
 
 function setClock(selector, endtime) {
+  const timer = document.querySelector(selector),
+    days = timer.querySelector("#days"),
+    hours = timer.querySelector("#hours"),
+    minutes = timer.querySelector("#minutes"),
+    seconds = timer.querySelector("#seconds"),
+    timeInterval = setInterval(updateClock, 1000);
 
-    const timer = document.querySelector(selector),
-        days = timer.querySelector("#days"),
-        hours = timer.querySelector('#hours'),
-        minutes = timer.querySelector('#minutes'),
-        seconds = timer.querySelector('#seconds'),
-        timeInterval = setInterval(updateClock, 1000);
+  updateClock();
 
-    updateClock();
+  function updateClock() {
+    const t = getTimeRemaining(endtime);
 
-    function updateClock() {
-        const t = getTimeRemaining(endtime);
+    days.innerHTML = getZero(t.days);
+    hours.innerHTML = getZero(t.hours);
+    minutes.innerHTML = getZero(t.minutes);
+    seconds.innerHTML = getZero(t.seconds);
 
-        days.innerHTML = getZero(t.days);
-        hours.innerHTML = getZero(t.hours);
-        minutes.innerHTML = getZero(t.minutes);
-        seconds.innerHTML = getZero(t.seconds);
-
-        if (t.total <= 0) {
-            clearInterval(timeInterval);
-        }
+    if (t.total <= 0) {
+      clearInterval(timeInterval);
     }
+  }
 }
 
-setClock('.timer', deadline);
+setClock(".timer", deadline);
 
+$(document).ready(function () {
+  $(".products-cards").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
 
+    nextArrow: '<i class="fa fa-angle-right" aria-hidden="true"></i>',
+    prevArrow: '<i class="fa fa-angle-left" aria-hidden="true"></i>',
 
-// $(document).ready(function(){
-//     $('.multi-split').slick({
-//         slidesToShow: 4,
-//         slidesToScroll: 1,
-//       arrows: true,
-      
-      
-//       nextArrow: '<i class="fa fa-angle-right" aria-hidden="true"></i>',
-//       prevArrow: '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-      
-//       responsive: [
-//   {
-//     breakpoint: 1024,
-//     settings: {
-//       slidesToShow: 4,
-//       slidesToScroll: 1,    
-
-//     }
-//   },
-//   {
-//     breakpoint: 600,
-//     settings: {
-//       slidesToShow: 1,
-//       slidesToScroll: 1
-//     }
-//   },
-//   {
-//     breakpoint: 480,
-//     settings: {
-//       slidesToShow: 1,
-//       slidesToScroll: 1
-
-//     }
-//   }
-
-// ]
-//     });
-//   });
+    responsive: [
+      {
+        breakpoint: 1026,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
