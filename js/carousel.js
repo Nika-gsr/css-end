9d833ec94369c1a0a7c6a3df38f61bd7fc1ab649
Nika Nebesna <kreuzfeuer8768@gmail.com>
2020-11-30 02:34:35 +0200

    End

--- js/carousel.js
+++ js/carousel.js
@@ -2,7 +2,7 @@ const images = [
     `<div class="carousel-item active">
     <img src="img/blog/blog-wind.png" class="d-block" alt="health">
     <div class="carousel-caption">
-      <a href="blog.html#defect">
+      <a href="blog.html##health-article">
         <h5>Чи шкідливий кондиціонер для здоров’я?</h5>
         <p>Значну частину нашого життя ми проводимо в замкнутому просторі: офісі, супермаркетах, квартирі або котеджі.</p>
       </a>
@@ -11,7 +11,7 @@ const images = [
     `<div class="carousel-item active">
     <img src="img/blog/blog-grey.png" class="d-block" alt="...">
     <div class="carousel-caption">
-      <a href="blog.html#sick">
+      <a href="blog.html#sick-article">
         <h5>Поради, як не захворіти від кондиціонера</h5>
         <p>Незважаючи на шалену популярність кондиціонерів, їх і досі бояться постійно вмикати, щоб не захворіти. </p>
       </a>
@@ -20,7 +20,7 @@ const images = [
     `<div class="carousel-item active">
     <img src="img/blog/blog-sofa.png" class="d-block" alt="...">
     <div class="carousel-caption">
-      <a href="blog.html#false">
+      <a href="blog.html#myth-article">
         <h5>Міфи про кондиціонери</h5>
         <p>Серед усієї побутової техніки кондиціонери чи не найбільше обросли величезною кількістю міфів.
         </p>
